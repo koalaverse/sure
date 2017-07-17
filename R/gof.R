558be4e3ff062a51ec0507ec4ec5d20cfe9dcaa2
Brandon Greenwell <greenweb@illuminationworksllc.com>
2017-07-17 11:44:55 -0400

    expanded to include AD/CVM GOF tests

---
+++ R/gof.R
@@ -0,0 +1,98 @@
+#' Kolmogorov-Smirnov Simulation
+#'
+#' Simulate p-values from a goodness-of-fit test (experimental).
+#'
+#' @param object An object of class \code{\link[ordinal]{clm}},
+#' \code{\link[MASS]{polr}}, or \code{\link[VGAM]{vglm}}.
+#'
+#' @param nsim Integer specifying the number of bootstrap replicates to use.
+#'
+#' @param test Cahracter string specifying which goodness-of-fit test to use.
+#' Options include: \code{"ks"} for the Kolmogorov-Smirnov test; \code{"ad"} for
+#' the Anderson-Darling test, and \code{"cvm"} for the Cramer-Von Mises test.
+#' Default is \code{"ks"}.
+#'
+#' @param ... Additional optional arguments. (Currently ignored.)
+#'
+#' @param x An object of class \code{"gof"}.
+#'
+#' @return An object of class \code{c("gof", "numeric")}.
+#'
+#' @details
+#' Under the null hypothesis, the distribution of the p-values should appear
+#' uniformly distributed on the interval [0, 1]. This can be visually
+#' investigated using the \code{plot} method. A 45 degree line is indicative of
+#' a "good" fit.
+#'
+#' @rdname gof
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' # Load required packages
+#' library(MASS)
+#' library(ordr)
+#'
+#' # Fit a proportional odds model
+#' house.polr <- polr(Sat ~ Infl + Type + Cont, weights = Freq, data = housing,
+#'                    method = "logistic")
+#'
+#' # Goodness-of-fit
+#' plot(gof(house.polr, nsim = 1000))
+#' }
+gof <- function(object, nsim = 10, test = c("ks", "ad", "cvm"), ...) {
+  if (nsim <- as.integer(nsim) < 2) {
+    stop("nsim must be a postive integer >= 2")
+  }
+  UseMethod("gof")
+}
+
+
+#' @keywords internal
+sim.pvals <- function(res, test, pfun) {
+  gof.test <- switch(test, "ks" = stats::ks.test, "ad" = goftest::ad.test,
+                     "cvm" = goftest::cvm.test)
+  pvals <- apply(attr(res, "boot.reps"), MARGIN = 2, FUN = function(x) {
+    gof.test(x, pfun)$p.value
+  })
+  class(pvals) <- c("gof", "numeric")
+  pvals
+}
+
+
+#' @rdname gof
+#' @export
+gof.clm <- function(object, nsim = 10, test = c("ks", "ad", "cvm"), ...) {
+  res <- resids(object, nsim = nsim)
+  test <- match.arg(test)
+  pfun <- getDistributionFunction(object)
+  sim.pvals(res, test = test, pfun = pfun)
+}
+
+
+#' @rdname gof
+#' @export
+gof.polr <- function(object, nsim = 10, test = c("ks", "ad", "cvm"), ...) {
+  res <- resids(object, nsim = nsim)
+  test <- match.arg(test)
+  pfun <- getDistributionFunction(object)
+  sim.pvals(res, test = test, pfun = pfun)
+}
+
+
+#' @rdname gof
+#' @export
+gof.vglm <- function(object, nsim = 10, test = c("ks", "ad", "cvm"), ...) {
+  res <- resids(object, nsim = nsim)
+  test <- match.arg(test)
+  pfun <- getDistributionFunction(object)
+  sim.pvals(res, test = test, pfun = pfun)
+}
+
+
+#' @rdname gof
+#' @export
+plot.gof <- function(x, ...) {
+  graphics::plot(stats::ecdf(x), xlab = "p-value", xlim = c(0, 1), ...)
+  graphics::abline(0, 1, lty = 2)
+}
